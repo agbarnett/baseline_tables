cf0d11a9f965dfbf4132a5f8923bd7ace6baf5f0
Adrian Barnett <a.barnett@qut.edu.au>
2022-06-10 21:22:47 +1000

    gitignore

--- temp3.R
+++
@@ -1,8 +0,0 @@
-# get the multiplier 
-flag_stats = filter(all_stats, nicevar=='Precision') %>%
-  left_join(table_summaries, by='pmcid') %>% # add table features
-  dplyr::filter(n_cols ==2,
-         lower > 0) %>% # lower positive (high precision)
-  left_join(design, by='pmcid')
-#
-flag_stats$pmcid
